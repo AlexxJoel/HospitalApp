352d2b64748f7b6186a7248158130c6432f05915
AlexxJoel <20213tn019@utez.edu.com>
2025-05-09 13:58:08 -0600

    add HTML interface for creating and listing medical appointments with filtering options

--- HOSPITAL_API/src/main/java/com/example/demo/controller/MedicalAppointmentController.java
+++ HOSPITAL_API/src/main/java/com/example/demo/controller/MedicalAppointmentController.java
@@ -4,6 +4,7 @@ import com.example.demo.controller.dtos.CreateAppointmentRequest;
 import com.example.demo.model.services.MedicalAppointmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import java.time.format.DateTimeParseException;
 @RestController
 @RequestMapping("/appointments")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class MedicalAppointmentController {
     private final MedicalAppointmentService medicalAppointmentService;
 
